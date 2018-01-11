840d816bcd9e0b4a3742f7dba81b858200bd3175
shubhamknoldus <shubham.verma@knoldus.in>
2018-01-11 22:58:07 +0530

    Both codes perfectly working

--- src/main/scala/com/knoldus/application/ApplicationObject.scala
+++ src/main/scala/com/knoldus/application/ApplicationObject.scala
@@ -8,6 +8,13 @@ object ApplicationObject extends App {
 
   val arg1 = 6
   val arg2 = 7
+  val arg3 = 3
+  val arg4 = 6
+  val arg5 = 2
+  val arg6 = 1
+  val arg7 = 9
+  val arg8 = 4
+
   val logger = Logger.getLogger(this.getClass)
   val sumSquare = (a: Int, b: Int) => ((a * a) + (b * b))
   val sumCubes = (a: Int, b: Int) => ((a * a * a) + (b * b * b))
@@ -19,17 +26,13 @@ object ApplicationObject extends App {
   logger.info(sumObject.sum(sumCubes, arg1, arg2) + "\n")
   logger.info(sumObject.sum(sumInts, arg1, arg2) + "\n")
 
-  val arg3 = 34
-  val arg8 = 43
-  val arg7 = 97
-  val arg6 = 18
-  val arg5 = 21
-  val arg4 = 62
 
   val list = List(arg2,arg1,arg3,arg4,arg5,arg6,arg7,arg8)
   val listOper: List[String] = List("sum","product","max")
 
   val obj = new ListOperations
-  logger.info(obj.operateList(list, listOper(0)))
+  logger.info(obj.operateList(list, listOper(0)) + "\n")
+  logger.info(obj.operateList(list, listOper(2)) + "\n")
+  logger.info(obj.operateList(list, listOper(1)) + "\n")
 
 }
--- src/main/scala/com/knoldus/operation/ListOperations.scala
+++ src/main/scala/com/knoldus/operation/ListOperations.scala
@@ -6,26 +6,30 @@ class ListOperations {
 
     val sum = (a: Int,b: Int) => a + b
     val product = (a: Int, b: Int) => a * b
+    val max = (a: Int, b: Int) => if(a >= b) {
+      a
+    } else {
+      b
+    }
 
     operation.toLowerCase match {
       case "sum" => {
-        recurseList(list(0), list, sum)
+        recurseList(0, list, sum)
       }
       case "product" => {
-        recurseList(list(0), list, product)
+        recurseList(1, list, product)
       }
-     /* case "max" => {
-      }*/
+      case "max" => {
+        recurseList(list(0), list, max)
+      }
     }
   }
 
   def recurseList(compute: Int, list:  List[Int], f: (Int, Int) => Int): Int =
   {
       list match {
-        case head :: head2 :: tail => recurseList(f(head, head2), tail, f)
-        case head :: head2 :: Nil => f(head2,compute) //why only this is not working
-        case head :: Nil => compute
         case Nil => compute
+        case head :: tail => recurseList(f(compute, head), tail, f)
       }
   }
 }
